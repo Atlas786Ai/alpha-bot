0252fda8fb2d5386eafca23bd57222d647da97c4
Atlas786Ai <297103450+Atlas786Ai@users.noreply.github.com>
2026-07-02 02:05:58 +0330

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,8 +1,30 @@
-def run_engine():
-    return {
-        "top10": [
-            {"symbol": "SUI", "score": 0.93},
-            {"symbol": "INJ", "score": 0.91},
-            {"symbol": "SEI", "score": 0.90}
-        ]
+import requests
+
+def get_market():
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": 10,
+        "page": 1
     }
+    return requests.get(url, params=params).json()
+
+
+def run_engine():
+
+    data = get_market()
+
+    results = []
+
+    for c in data:
+        results.append({
+            "symbol": c["symbol"],
+            "score": (
+                (c.get("price_change_percentage_24h", 0) or 0)
+            )
+        })
+
+    results.sort(key=lambda x: x["score"], reverse=True)
+
+    return {"top10": results}
