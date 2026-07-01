f2d2774b1b72c742aee5e898fbefbf77f70c869f
Atlas786Ai <297103450+Atlas786Ai@users.noreply.github.com>
2026-07-02 01:46:32 +0330

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,8 @@
+def run_engine():
+    return {
+        "top10": [
+            {"symbol": "SUI", "score": 0.93},
+            {"symbol": "INJ", "score": 0.91},
+            {"symbol": "SEI", "score": 0.90}
+        ]
+    }
